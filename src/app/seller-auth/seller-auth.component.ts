e88968eaf94b606694cdff4e1b6907de9da6f42e
Sahil pillania <sahilpillania88@gmail.com>
2023-06-24 12:35:54 +0530

    signup api added

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { SellerService } from '../services/seller.service';
+import { Router } from '@angular/router';
+import { signUp } from 'src/data-type';
 
 @Component({
   selector: 'app-seller-auth',
   templateUrl: './seller-auth.component.html',
-  styleUrls: ['./seller-auth.component.css']
+  styleUrls: ['./seller-auth.component.css'],
 })
 export class SellerAuthComponent implements OnInit {
-  
-  constructor() {}
+  constructor(private seller: SellerService, private route: Router) {}
 
   ngOnInit(): void {}
-  signUp(data:object):void{
-    console.warn(data)
+  signUp(data: signUp): void {
+    this.seller.userSignUp(data).subscribe((result) => {
+      console.log('data returned', result);
+      this.route.navigate(['seller-home']);
+    });
   }
-
 }
