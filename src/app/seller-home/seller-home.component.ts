07c0e0a11b17fc006be84893e00ea46bacaadbb3
Sahil pillania <sahilpillania88@gmail.com>
2023-06-25 12:31:10 +0530

    added listing or seller products

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ProductService } from '../services/product.service';
+import { product } from 'src/data-type';
 
 @Component({
   selector: 'app-seller-home',
   templateUrl: './seller-home.component.html',
-  styleUrls: ['./seller-home.component.css']
+  styleUrls: ['./seller-home.component.css'],
 })
-export class SellerHomeComponent {
+export class SellerHomeComponent implements OnInit {
+  productList: undefined | product[];
+  constructor(private product: ProductService) {}
 
+  ngOnInit(): void {
+    this.product.productList().subscribe((result) => {
+      console.warn(result);
+      if (result) {
+        this.productList = result;
+      }
+    });
+  }
 }
