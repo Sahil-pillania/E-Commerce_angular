bf09ff7d0b10e1634c4a71d88ee7888441cdff9b
Sahil pillania <sahilpillania88@gmail.com>
2023-06-25 10:45:25 +0530

    Auth guard for seller

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -11,11 +11,10 @@ import { signUp } from 'src/data-type';
 export class SellerAuthComponent implements OnInit {
   constructor(private seller: SellerService, private route: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.seller.reloadSeller();
+  }
   signUp(data: signUp): void {
-    this.seller.userSignUp(data).subscribe((result) => {
-      console.log('data returned', result);
-      this.route.navigate(['seller-home']);
-    });
+    this.seller.userSignUp(data);
   }
 }
