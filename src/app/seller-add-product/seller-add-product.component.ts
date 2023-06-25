e1d688804cba2b6d03c4b0637c2460b12c4c85b0
Sahil pillania <sahilpillania88@gmail.com>
2023-06-25 12:00:18 +0530

    Add product UI

---
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-add-product',
+  templateUrl: './seller-add-product.component.html',
+  styleUrls: ['./seller-add-product.component.css'],
+})
+export class SellerAddProductComponent {
+  constructor() {}
+
+  ngOnInit() {}
+
+  submit(data: object) {
+    console.warn(data);
+  }
+}
