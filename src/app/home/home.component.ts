ab37cbb7e4010f7815f2bc999d02f36ed4f6206b
Sahil pillania <sahilpillania88@gmail.com>
2023-06-27 20:32:46 +0530

    product API added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,11 +10,16 @@ import { product } from 'src/data-type';
 export class HomeComponent {
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   popularProducts: undefined | product[];
+  trendyProducts: undefined | product[];
   constructor(private product: ProductService) {}
 
   ngOnInit(): void {
     this.product.popularProducts().subscribe((data) => {
       this.popularProducts = data;
+    });
+
+    this.product.trendyProducts().subscribe((data) => {
+      this.trendyProducts = data;
     });
   }
 }
