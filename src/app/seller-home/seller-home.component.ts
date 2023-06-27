8224d144190af27680e1ed9ec4bca53a7b69230a
Sahil pillania <sahilpillania88@gmail.com>
2023-06-27 19:33:26 +0530

    Added fontawesome

--- src/app/seller-home/seller-home.component.ts
+++ src/app/seller-home/seller-home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ProductService } from '../services/product.service';
 import { product } from 'src/data-type';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-seller-home',
@@ -11,6 +12,7 @@ import { product } from 'src/data-type';
 export class SellerHomeComponent implements OnInit {
   productList: undefined | product[];
   productMessage: undefined | string;
+  icon = faTrash;
   constructor(private product: ProductService) {}
 
   ngOnInit(): void {
