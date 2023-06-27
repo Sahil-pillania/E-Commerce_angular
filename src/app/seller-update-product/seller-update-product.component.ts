c1e1bcaed39c93fb9559fb5389570745e265cfb7
Sahil pillania <sahilpillania88@gmail.com>
2023-06-27 19:57:04 +0530

    Done with update API

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -11,6 +11,7 @@ import { product } from 'src/data-type';
 })
 export class SellerUpdateProductComponent implements OnInit {
   productData: undefined | product;
+  productMessage: undefined | string;
   constructor(private route: ActivatedRoute, private product: ProductService) {}
 
   ngOnInit(): void {
@@ -22,5 +23,18 @@ export class SellerUpdateProductComponent implements OnInit {
         this.productData = data;
       });
   }
-  submit(data: any) {}
+  submit(data: any) {
+    if (this.productData) {
+      data.id = this.productData.id;
+    }
+    this.product.updateProduct(data).subscribe((result) => {
+      if (result) {
+        this.productMessage = 'Product has updated';
+      }
+    });
+    setTimeout(() => {
+      this.productMessage = undefined;
+    }, 3000);
+    console.warn(data);
+  }
 }
