d2064835060a9fd0011dcd7a9d104eac414f21a6
Sahil pillania <sahilpillania88@gmail.com>
2023-06-28 09:10:38 +0530

    user signup API

--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { signUp } from 'src/data-type';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-user-auth',
@@ -7,11 +8,11 @@ import { signUp } from 'src/data-type';
   styleUrls: ['./user-auth.component.css'],
 })
 export class UserAuthComponent implements OnInit {
-  constructor() {}
+  constructor(private user: UserService) {}
 
   ngOnInit(): void {}
 
   signUp(data: signUp) {
-    console.warn(data);
+    this.user.userSignUp(data);
   }
 }
